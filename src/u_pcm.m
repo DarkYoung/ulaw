765842f78f9cd6486503e4f29daf12da986c2c02
Jon Zhang <16302010059@fudan.edu.cn>
2018-11-05 16:08:51 +0800

    update

--- src/u_pcm.m
+++ src/u_pcm.m
@@ -9,7 +9,7 @@ a_max = max(abs(a)); % 找到采样点的最大值
 a_quan = a ./ a_max; % 将采样点映射到(-1, 1)
 for i = -1 : 2 / n : 1 % 将(-1, 1)分成n段
     a_quan_seg = a_quan(a_quan >= i & a_quan < (i + 2 / n)); % 获取在(i, i + 2 / n)范围内的采样点
-    a_quan(a_quan >= i & a_quan < (i + 2 / n)) = (max(a_quan_seg) + min(a_quan_seg)) / 2; % 将这些采样点设为（最大值 + 最小值） / 2
+    a_quan(a_quan >= i & a_quan < (i + 2 / n)) = a_max * (max(a_quan_seg) + min(a_quan_seg)) / 2; % 将这些采样点设为（最大值 + 最小值） / 2
 end
     
 end
