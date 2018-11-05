1ba832b4e8956e2b5e2d5c51d0808bb0b1521fa2
Jon Zhang <16302010059@fudan.edu.cn>
2018-11-05 16:09:28 +0800

    update

--- src/ula_pcm.m
+++ src/ula_pcm.m
@@ -8,5 +8,5 @@ function [a_quan]=ula_pcm(a,n,u)
 
 x = ulaw(a, u);     % 经过u律压缩函数
 y = u_pcm(x, n);    % 经过均匀压缩函数
-a_quan = inv_ulaw(y, u) * max(abs(a)); % u律压缩逆运算，均匀压缩逆运算（不完全，仅乘模拟输入的最大值）
+a_quan = inv_ulaw(y, u); % u律压缩逆运算
 end
